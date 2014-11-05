92bd927550574ddbd29c5f002bdd792b274d1a8a
Allen Welkie <allen.welkie@link-labs.com>
2014-11-05 09:43:03 -0500

    Made setup scripts executable

--- install_vundle.sh
+++ install_vundle.sh
@@ -1,2 +1,3 @@
+#! /bin/bash
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 ln -s -i $DIR/.vimrc ~/.vimrc
 ln -s -i $DIR/.gvimrc ~/.gvimrc
