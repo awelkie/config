61d4649d785795755795cfa2790d7b6c2f9315b1
Allen Welkie <allen.welkie@link-labs.com>
2014-11-05 09:19:32 -0500

    Added some scripts

---
+++ install_vundle.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
---
+++ setup.sh
@@ -0,0 +1,4 @@
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ln -s -i $DIR/.vimrc ~/.vimrc
+ln -s -i $DIR/.gvimrc ~/.gvimrc
+ln -s -i $DIR/.tmux.conf ~/.tmux.conf
