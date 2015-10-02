f0872b98ec3dec519ddc11e1ab26e321dee85a9c
Allen Welkie <allen@localhost.localdomain>
2015-10-02 15:19:51 -0400

    Added vimperator config to hide status bar

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s -i $DIR/.vimrc ~/.vimrc
 ln -s -i $DIR/.gvimrc ~/.gvimrc
 ln -s -i $DIR/.tmux.conf ~/.tmux.conf
 ln -s -i $DIR/.ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -s -i $DIR/.vimperatorrc ~/.vimperatorrc
