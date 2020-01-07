016b137123efae29d025a409a041f74579f423dd
Allen Welkie <allen.welkie@snips.ai>
2020-01-07 10:50:05 +0100

    Remove files from setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-ln -s -i $DIR/.vimrc ~/.vimrc
 mkdir ~/.config/nvim
 ln -s -i $DIR/.vimrc ~/.config/nvim/init.vim
-ln -s -i $DIR/.gvimrc ~/.gvimrc
 ln -s -i $DIR/.tmux.conf ~/.tmux.conf
-ln -s -i $DIR/.ycm_extra_conf.py ~/.ycm_extra_conf.py
-ln -s -i $DIR/.vimperatorrc ~/.vimperatorrc
